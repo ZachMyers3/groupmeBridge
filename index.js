2ac0166a09fefaf5cca1afa8fb7f91c8bfc6faf5
Zach Myers <zachmyers3@gmail.com>
2017-04-17 13:30:24 -0400

    changed port

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+var Cli         = require("matrix-appservice-bridge").Cli;
+var Bridge      = require("matrix-appservice-bridge").Bridge;
+var AppServiceRegistration 
+                = require("matrix-appservice-bridge").AppServiceRegistration;
 var http        = require("http");
 var requestLib  = require("request");
-var bridge;
 
-const PORT = 8585;
+const PORT = 9889;
+
+new Cli({
+  registrationPath: "groupme-registration.yaml",
+  generateRegistration: function(reg, callback) {
+    reg.setId(AppServiceRegistration.generateToken());
+    reg.setHomeserverToken(AppServiceRegistration.generateToken());
+    reg.setAppServiceToken(AppServiceRegistration.generateToken());
+    reg.setSenderLocalpart("groupmebot");
+    reg.addRegexPattern("users", "@groupme_.*", true);
+    callback(reg);
+  },
+  run: function(port, config) {
+    // TODO
+  }
+}).run();
 
 http.createServer(function (request, response) {
   console.log(request.method + " " + request.url);
