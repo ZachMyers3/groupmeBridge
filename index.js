319e7f40766e85b7c33a547c384fb179d7d58414
Zach Myers <zachmyers3@gmail.com>
2017-04-24 14:28:50 -0400

    removed post from hook uri

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var qs 		= require("querystring");
 
 const PORT = 9889;
 const ROOM_ID = "!zeGfOsnOVRaFQzfljM:gmbridge.ddns.net";
-const GROUPME_WEBHOOK_URL = "https://api.groupme.com/v3/bots/post"
+const GROUPME_WEBHOOK_URL = "https://api.groupme.com/v3/bots/"
 
 http.createServer(function (request, response) {
   console.log(request.method + " " + request.url);
