afb3f16f7740b9a8693f78235fffee64f8160860
Zach Myers <zachmyers3@gmail.com>
2017-04-24 14:25:31 -0400

    added requestlib for groupme api

--- index.js
+++ index.js
@@ -74,10 +74,11 @@ new Cli({
                         json: true,
                         uri: GROUPME_WEBHOOK_URL,
                         body: {
-                            username: event.user_id,
+                            bot_id: event.user_id,
                             text: event.content.body
                         }
                     }, function(err, res) {
+                        console.log(res);
                         if (err) {
                             console.log("HTTP Error: %s", err);
                         }
@@ -93,4 +94,37 @@ new Cli({
     }
 }).run();
 
+function postMessage() {
+  var botResponse, options, body, botReq;
 
+  botResponse = cool();
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
